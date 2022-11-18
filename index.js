0f8312df9299149d9d818547ccdde77c9b5e2c1e
Brian <mumobrian2746@gmail.com>
2022-11-18 06:08:46 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,32 @@
+function introduction(name){
+
+    return `Hi, my name is ${name}.`;
+    
+};
+
+introduction(Aki);
+
+function introductionWithLanguage(name, language){
+
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+
+};
+
+introductionWithLanguage(Aki,Ember.js);
+
+function introductionWithLanguageOptional(name, language){
+
+    if(language===null){
+
+        return `Hi, my name is ${name} and I am learning to program in Javascript.`
+
+    }
+    else{
+        return `Hi, my name is ${name} and I am learning to program in ${language}.`
+    }
+
+};
+
+introductionWithLanguageOptional(Gracie);
+introductionWithLanguageOptional(Gracie, Python);
+
